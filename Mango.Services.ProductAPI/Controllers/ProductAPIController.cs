fc30b8a1466f235131accee5e1662b9249f3d64b
Viktor Tseplyayev <viktortseplyayev@tengizchevroil.com>
2023-03-29 17:47:02 +0500

    New Controller

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mango.Services.ProductAPI.Models.DTO;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDTO response;
+        private IProductRepository productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+            this.response = new ResponseDTO();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDTO> productDTOs = await productRepository.GetProducts();
+                response.Result = productDTOs;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDTO productDTO = await productRepository.GetProductById(id);
+                response.Result = productDTO;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return response;
+        }
+    }
+}
