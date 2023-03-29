2b43f2c67e9517b157a6967fc24be15b46cb27d0
Viktor Tseplyayev <viktortseplyayev@tengizchevroil.com>
2023-03-29 16:26:58 +0500

    DB Connection with Migration

---
+++ Mango.Services.ProductAPI/Contexts/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Contexts
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
